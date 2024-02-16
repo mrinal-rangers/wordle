e5c405d6769968c9f82a88b2077bb71d73bc97fd
Mrinal Deb <mrinal437a@gmail.com>
2024-02-16 22:09:04 +0530

    added the clear functionality

--- script.js
+++ script.js
@@ -8,23 +8,36 @@ keys.forEach((key)=>{
     key.addEventListener('click',()=>{
         const text = key.innerHTML;
         if(text ==="ENTER"){
-            if(length ===5){
+            if(length ===5 ){
                 attempt ++;
                 s="";
                 length = 0;
             }
-            return ;
-            
+            return ;  
         }
-        if(length<5){
+        else if(text === "CLEAR"){
+           if(length!=0){
+            length--;
+            s = s.slice(0, -1); 
+            }
+        }    
+        else if(length<5){
             length++;
             s+=text;
         }
         const that_row = document.querySelector(`.row${attempt}`);
-        for(let index= 0;index<s.length; index++){
+        console.log(that_row);
+        for(let index= 0;index<5; index++){
             const that_col = that_row.querySelector(`.column${index+1}`);
-            that_col.innerHTML = s[index];
-            that_col.classList.add('selected');
+            console.log(that_col);
+            if (index < length) {
+                that_col.innerHTML = s[index];
+                that_col.classList.add('selected'); 
+            } else {
+                that_col.innerHTML = "";
+                that_col.classList.remove('selected'); 
+            }
+
         }
     })
 })
